5b664ec7f692a81f0247220557f8ecb8aa457816
goose <anrobbins18@gmail.com>
2023-04-11 13:15:20 -0400

    updates on link.js file

--- Assets/js/link.js
+++ Assets/js/link.js
@@ -1,4 +1,4 @@
-var goToHsPage = document.querySelector("#HS-page");
+var fromHomeToHighscores = document.querySelector("#Hs-page");
 var navBar = document.querySelector("#home");
 var timer = document.querySelector(".timer");
 var timeDisplayed = document.querySelector(".time")
@@ -19,7 +19,14 @@ var clearHsBtn = document.querySelector(".clear-highscores");
 var highScoresPage = [viewHighscores, listHighScores, goBackBtn, clearHsBtn];
 
 function navigateHighscores (event){
-    //this makes the finished game page hidden
+    event.preventDefault();
+    //already an array - gets what is in local storage already (from past games) or sets it to an empty array
+    var highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+    //console.log to see all high scores
+    console.log(highscores);
+    //sort array from high score to lowest score
+    highscores.sort((a, b) => b.score - a.score);
+    //this makes the finishedGamepage hidden
     for (var i=0; i<finishedGamePage.length; i++){
         finishedGamePage[i].classList.add("hidden");
         finishedGamePage[i].classList.remove("visible", "timer", "time", "final-score-message", "submit-final-score", "initials", "submitBtn");
@@ -29,6 +36,17 @@ function navigateHighscores (event){
     for (var i=0; i<highScoresPage.length; i++){
         highScoresPage[i].classList.add("visible");
         highScoresPage[i].classList.remove("hidden");
+    }         
+    for (var i=0; i<highscores.length;i++){
+        //creates the li element
+        var newListItem = document.createElement("li");
+        console.log(newListItem);
+        //adds the high score text to the li
+        newListItem.textContent = (i + 1) + ". " + highscores[i].initials + " " + "-" + " " + highscores[i].score;
+        //give the newly created li a class of high-score-li so it can be styled in the CSS
+        newListItem.setAttribute("class", "high-score-li")
+        //add new element to the DOM
+        listHighScores.appendChild(newListItem);
     }
 }
-goToHsPage.addEventListener("click", navigateHighscores);
+fromHomeToHighscores.addEventListener("click",navigateHighscores);
