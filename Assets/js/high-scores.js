ddb3fad973ef0315a09b0d2634e339a2eacad1c6
goose <anrobbins18@gmail.com>
2023-04-11 11:21:05 -0400

    added comments and rearranged high-scores.js functions

--- Assets/js/high-scores.js
+++ Assets/js/high-scores.js
@@ -18,13 +18,10 @@ var goBackBtn = document.querySelector(".back-to-home");
 var clearHsBtn = document.querySelector(".clear-highscores");
 //all high score page elements in an array
 var highScoresPage = [viewHighscores, listHighScores, goBackBtn, clearHsBtn];
-//undefined initials variable
+//timeLeft is getting the time-left value stored in localStorage set from script.js
 var timeLeft = localStorage.getItem("time-left")
 
 
-function saveHighScore () {
-   
-    }
 //length + 1
 //go to high scores page
 //loop through what is in highscores array
@@ -34,38 +31,44 @@ function goToHighScores(event){
     event.preventDefault();
     var initials = inputTextArea.value.trim();
     if(initials !== ''){
-        //template
+        //template - set final score initials to input initials and score to timeLeft
         var finalScore = {
             initials: initials,
             score: timeLeft
         }
-        //already an array
+        //already an array - gets what is in local storage or sets it to an empty array
         var highscores = JSON.parse(localStorage.getItem('highscores')) || [];
-        //pushing the template into existing array (highscores) 
+        //pushing the template into the existing array (highscores), so we are adding the new finalScore to the highscores array
         highscores.push(finalScore);
+        //now we are updating the high scores in localStorage
         localStorage.setItem("highscores", JSON.stringify(highscores))
+        //console.log to see all high scores
         console.log(highscores);
+        //sort array from high score to lowest score
+        highscores.sort((a, b) => b.score - a.score);
+        //this makes the finishedGamepage hidden
+        for (var i=0; i<finishedGamePage.length; i++){
+            finishedGamePage[i].classList.add("hidden");
+            finishedGamePage[i].classList.remove("visible", "timer", "time", "final-score-message", "submit-final-score", "initials", "submitBtn");
 
-    for (var i=0; i<finishedGamePage.length; i++){
-        finishedGamePage[i].classList.add("hidden");
-        finishedGamePage[i].classList.remove("visible", "timer", "time", "final-score-message", "submit-final-score", "initials", "submitBtn");
-
-    }
-    //this makes the high scores page visible
-    for (var i=0; i<highScoresPage.length; i++){
-        highScoresPage[i].classList.add("visible");
-        highScoresPage[i].classList.remove("hidden");
-}         
-        for (var i=0; i<highscores[i].length;i++)
+        }
+        //this makes the high scores page visible
+        for (var i=0; i<highScoresPage.length; i++){
+            highScoresPage[i].classList.add("visible");
+            highScoresPage[i].classList.remove("hidden");
+        }         
+        for (var i=0; i<highscores[i].length;i++){
             //creates the li element
             var newListItem = document.createElement("li");
             console.log(newListItem);
             //adds the high score text to the li
-            newListItem.textContent = highscores.initials.length + ". " + highscores.initials[i] + " " + "-" + " " + highscores.score[i];
+            newListItem.textContent = highscores[i].length + ". " + highscores.initials[i] + " " + "-" + " " + highscores.score[i];
             //give the newly created li a class of high-score-li so it can be styled in the CSS
             newListItem.setAttribute("class", "high-score-li")
             //add new element to the DOM
             listHighScores.appendChild(newListItem);
+        }
+
     } 
 }
 function goHome(){
@@ -76,8 +79,7 @@ function clearHighScores(){
     //removes all highscore lis
     document.querySelector(".high-score-li").remove();
 }
-//save the high score
-saveHighScore();
+
 //put timeLeft value in finalScoreMessage and timer
 timeDisplayed.textContent = timeLeft;
 finalScoreDisplayed.textContent = timeLeft;
